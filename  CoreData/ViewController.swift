720afb752c37c14a4575fefb0b7eaf5c5860f063
Rafael <trasmont@gmail.com>
2018-01-05 19:40:58 -0500

    commit Part 1

---
+++  CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//   CoreData
+//
+//  Created by Rafael M. Trasmontero on 1/5/18.
+//  Copyright © 2018 GTTuts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+
+
+}
+
